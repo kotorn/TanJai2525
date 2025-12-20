3de09e3dde8d472bb0a3e44bb41026335c8c370e
KOTORN <KOTORN@GMAIL.COM>
2025-12-20 14:49:07 +0700

    feat(auth): Add Sign Up tab and logic, fix oauth error handling

--- apps/web/src/features/auth/auth-handlers.ts
+++ apps/web/src/features/auth/auth-handlers.ts
@@ -65,3 +65,20 @@ export const signInWithEmail = async (email: string, password: string) => {
     return data;
 };
 
+/**
+ * Trigger Email Sign Up
+ */
+export const signUpWithEmail = async (email: string, password: string) => {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: `${window.location.origin}/auth/callback`,
+        },
+    });
+    if (error) throw error;
+    return data;
+};
+
+
